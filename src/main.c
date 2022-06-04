e0f8ce8f281e5a3640a6ac4396e4a8c131f72fc2
sasodoma <saso.doma@gmail.com>
2022-06-04 20:56:45 +0200

    Fix some alignment

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 /* FM video receiver module with Arduino Nano.
  * Uses an RX5808 in SPI mode and an SSD1306 I2C OLED display
  * Connection:
- *     SSD1306: SCK - A5
+ *  SSD1306:    SCK - A5
  *              SDA - A4
- *  RTC6715: CH1 (Data) - D11
+ *  RTC6715:    CH1 (Data) - D11
  *              CH2 (CS)   - D10
  *              CH3 (CLK)  - D13
  *              RSSI        - A0
- *  BUTTONS: T1-T4 - D4-D7
+ *  BUTTONS:    T1-T4 - D4-D7
  *              (buttons pull to GND)
  */
 
