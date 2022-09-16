127a984abe77a4fc8a7f27223ac5e097e9cf0083
sasodoma <saso.doma@gmail.com>
2022-09-16 18:21:12 +0200

    Code alignment

--- include/pins.h
+++ include/pins.h
@@ -4,18 +4,18 @@
 #include <avr/io.h>
 
 // Register level macro "functions"
-#define _setGpioOutput(port, pin)         DDR##port |=  (1 << pin)
-#define _setGpioInput(port, pin)          DDR##port &= ~(1 << pin), PORT##port &= ~(1 << pin)
-#define _setGpioInputPullup(port, pin)    DDR##port &= ~(1 << pin), PORT##port |=  (1 << pin)
-#define _setGpioHigh(port, pin)          PORT##port |=  (1 << pin)
-#define _setGpioLow(port, pin)           PORT##port &= ~(1 << pin)
+#define _setGpioOutput(port, pin)       DDR##port |=  (1 << pin)
+#define _setGpioInput(port, pin)        DDR##port &= ~(1 << pin), PORT##port &= ~(1 << pin)
+#define _setGpioInputPullup(port, pin)  DDR##port &= ~(1 << pin), PORT##port |=  (1 << pin)
+#define _setGpioHigh(port, pin)         PORT##port |=  (1 << pin)
+#define _setGpioLow(port, pin)          PORT##port &= ~(1 << pin)
 
 // Helper functions, so parameters match
-#define setGpioOutput(...)        _setGpioOutput(__VA_ARGS__)
-#define setGpioInput(...)         _setGpioInput(__VA_ARGS__)
-#define setGpioInputPullup(...)   _setGpioInputPullup(__VA_ARGS__)
-#define setGpioHigh(...)          _setGpioHigh(__VA_ARGS__)
-#define setGpioLow(...)           _setGpioLow(__VA_ARGS__)
+#define setGpioOutput(...)              _setGpioOutput(__VA_ARGS__)
+#define setGpioInput(...)               _setGpioInput(__VA_ARGS__)
+#define setGpioInputPullup(...)         _setGpioInputPullup(__VA_ARGS__)
+#define setGpioHigh(...)                _setGpioHigh(__VA_ARGS__)
+#define setGpioLow(...)                 _setGpioLow(__VA_ARGS__)
 
 // Pin definitions
 #define LED_BUILTIN D,1
